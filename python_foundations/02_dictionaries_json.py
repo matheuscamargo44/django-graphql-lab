39d137f2e9ed6eb24ef834b35037a6b0d9573282
Matheus Camargo <matheuscamargotech@gmail.com>
2025-12-12 17:49:42 -0300

    feat: add inventory list exercise
    Implement logic to store grades in lists inside a dictionary and calculate the average for each student.

--- python_foundations/02_dictionaries_json.py
+++ python_foundations/02_dictionaries_json.py
@@ -10,4 +10,18 @@ products = {
 }
 
 for product, price in products.items():
-    print(f"Product: {product} | Price: {price:.2f}")
+    print(f"Product: {product} | Price: {price:.2f}         ")
+
+# 2. Carregue um dicionário onde a chave seja o nome de um aluno e o valor seja uma lista contendo 3 notas desse aluno. No final, imprima as notas e as médias de cada aluno.
+# 2. Create a dictionary where the key is the student's name and the value is a list containing 3 grades. Finally, print the grades and the average for each student.
+
+gradebook = {  
+    "Matheus": [8.5, 9.0, 9.5],
+    "Ana": [7.0, 8.5, 3.0],
+    "Jorge": [6.0, 7.0, 6.5]
+}
+
+for student, grades in gradebook.items():
+    
+    average = sum(grades) / len(grades)
+    print(f"Student: {student} | Grades: {grades} | Average: {average:.2f}")
